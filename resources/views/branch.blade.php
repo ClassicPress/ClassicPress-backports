05f1b23cd2af33e0b2ff2f0e033f84042ed7c0ee
James Nylen <jnylen@gmail.com>
2018-10-07 00:47:40 -0500

    Update view

--- resources/views/branch.blade.php
+++ resources/views/branch.blade.php
@@ -29,25 +29,33 @@
             ClassicPress Bots
           </div>
           @if($user)
-          <div class="w-1/4 md:w-auto md:flex text-right">
-            <div>
-              <img class="inline-block h-8 w-8 rounded-full" src="{{$user->avatar}}" alt="">
-            </div>
-            <div class="hidden md:block md:flex md:items-center ml-2">
-              <span class="text-black text-sm mr-1">Signed in as {{$user->name}} | </span>
+            <div class="w-1/4 md:w-auto md:flex text-right">
+              <div>
+                <img class="inline-block h-8 w-8 rounded-full" src="{{$user->avatar}}" alt="">
+              </div>
+              <div class="hidden md:block md:flex md:items-center ml-2">
+                <span class="text-grey-dark text-sm mr-1">
+                  Logged in as
+                  <a
+                    href="https://github.com/{{$user->username}}"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >{{"@"}}{{$user->username}}</a>
+                  |
+                </span>
 
-              <a href="{{ route('logout') }}" class="text-black text-sm mr-1 hover:text-blue no-underline">Logout</a>
+                <a href="{{ route('logout') }}" class="text-black text-sm mr-1 hover:text-blue no-underline">Log out</a>
+              </div>
             </div>
-          </div>
           @else
-          <div class="w-1/4 md:w-auto md:flex text-right">
-            <div>
-              <img class="inline-block h-8 w-8 rounded-full" src="https://classicpress.assets-fider.io/images/100/120" alt="">
+            <div class="w-1/4 md:w-auto md:flex text-right">
+              <div>
+                <img class="inline-block h-8 w-8 rounded-full" src="https://classicpress.assets-fider.io/images/100/120" alt="">
+              </div>
+              <div class="hidden md:block md:flex md:items-center ml-2">
+                <a href="{{ route('login') }}" class="text-black text-sm mr-1 hover:text-blue no-underline">Log in</a>
+              </div>
             </div>
-            <div class="hidden md:block md:flex md:items-center ml-2">
-              <a href="{{ route('login') }}" class="text-black text-sm mr-1 hover:text-blue no-underline">Sign in</a>
-            </div>
-          </div>
           @endif
         </div>
       </div>
@@ -59,86 +67,97 @@
         <div class="flex-grow flex flex-col bg-white border-t border-b sm:rounded sm:border shadow overflow-hidden">
           <div class="border-b">
             <div class="flex justify-between px-6 -mb-px">
-              <h3 class="text-blue-dark py-4 font-normal text-lg">WordPress Commits</h3>
+              <h3 class="text-blue-dark py-4 font-normal text-lg">
+                WordPress commits:
+                <code>{{$branch}}</code> branch
+              </h3>
             </div>
           </div>
+
+          <!-- Commits list -->
           <?php foreach ($commits as $commit): ?>
-            @if($commit->status == 0)
+            <!-- Commit -->
             <div class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4">
-            @else
-            <div class="flex-grow flex px-6 py-2 text-grey-darker items-center -mx-4">
-            @endif
+              <!-- Commit hash and link -->
               <div class="w-2/5 xl:w-1/6 px-4 flex items-center">
-                <span class="text-lg"><a class="no-underline" href="{{$commit->html_link}}" target="_blank"><?php echo substr($commit->sha, 0, 7); ?></a></span>
+                <span class="text-lg">
+                  <a
+                    class="no-underline"
+                    href="{{$commit->html_link}}"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >{{substr($commit->sha, 0, 7)}}</a>
+              </span>
               </div>
+
+              <!-- Commit message -->
               <div class="hidden md:flex lg:hidden xl:flex w-2/3 px-4 items-center">
-                {{$commit->message}}
+                <pre class="commit-message">{{$commit->message}}</pre>
               </div>
+
+              <!-- Commit actions container -->
               <div class="flex w-1/6 md:w/12">
-
-              @if($user && $user->github_permissions['ClassicPress/ClassicPress'] == 'write' & $commit->status == 0)
-              <!-- Modal -->
-              <div class="modal fade" id="modal-{{$commit->sha}}" tabindex="-1" role="dialog" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title">Decline commit <?php echo substr($commit->sha, 0, 7); ?></h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-                      <form class="" action="index.html" method="post">
-                        <div class="form-group">
-                          <label for="reason">Reason for declining</label>
-                          <textarea class="form-control "name="reason" rows="8" cols="80"></textarea>
+                @if($user && $user->hasWriteAccess() && $commit->status == 0)
+                  <!-- Modal -->
+                  <div class="modal fade" id="modal-{{$commit->sha}}" tabindex="-1" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title">Decline commit <?php echo substr($commit->sha, 0, 7); ?></h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
                         </div>
-                        <div class="text-center">
-                          <button type="submit" class="btn btn-secondary">Confirm Decline</button>
-                          <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+                        <div class="modal-body">
+                          <form class="" action="index.html" method="post">
+                            <div class="form-group">
+                              <label for="reason">Reason for declining</label>
+                              <textarea class="form-control "name="reason" rows="8" cols="80"></textarea>
+                            </div>
+                            <div class="text-center">
+                              <button type="submit" class="btn btn-secondary">Confirm Decline</button>
+                              <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+                            </div>
+                          </form>
                         </div>
-                      </form>
-                    </div>
 
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-
-
-                <div class="inline-flex">
-                  <a href="http://dosomething.com"><button class="bg-blue hover:bg-grey text-white py-2 px-4 rounded-l">
-                    PR
-                  </button></a>
-                  <button data-toggle="modal" data-target="#modal-{{$commit->sha}}" class="bg-grey-light hover:bg-grey text-grey-darkest py-2 px-4 rounded-r">
-                    Decline
-                  </button>
-                </div>
-              @else
-              <div class="inline-flex">
-                <button class="bg-grey hover:bg-grey text-white py-2 px-4 rounded">
-                  Responded
-                </button>
-              </div>
-              @endif
-            </div>
-            </div>
-            @if($commit->status != 0)
-            <div class="px-3 py-4 border-b">
-              <div class="text-center text-grey">
-                @if($commit->status == 1)
-                  <a href="https://dosomething.com" class="no-underline text-grey" target="_blank">Pull Requested</a>
-                @else
-                  Pull Declined - Reason: {{$commit->decline_response}}
                 @endif
+
+                <!-- Commit actions -->
+                <div class="inline-flex">
+                  @if($commit->status == 0)
+                    <span class="text-grey">
+                      No action taken yet
+                    </span>
+                  @elseif($commit->status == 1)
+                    <a href="https://dosomething.com" class="no-underline text-grey" target="_blank">PR opened</a>
+                  @else
+                    Commit declined: {{$commit->decline_response}}
+                  @endif
+
+                  @if($user && $user->hasWriteAccess())
+                    @if($commit->status == 0)
+                      <a href="http://dosomething.com"><button class="bg-blue hover:bg-grey text-white py-2 px-4 rounded-l">
+                        PR
+                      </button></a>
+                      <button data-toggle="modal" data-target="#modal-{{$commit->sha}}" class="bg-grey-light hover:bg-grey text-grey-darkest py-2 px-4 rounded-r">
+                        Decline
+                      </button>
+                    @endif
+                  @endif
+                </div>
               </div>
-            </div>
-            @endif
+            </div><!-- End commit -->
           <?php endforeach; ?>
 
         </div>
       </div>
     </div>
   </div>
+
   <div class="bg-white border-t">
     <div class="container mx-auto px-4">
       <div class="md:flex justify-between items-center text-sm">
@@ -154,8 +173,5 @@
   </div>
 </div>
 
-
-
     </body>
-
 </html>
